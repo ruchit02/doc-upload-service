b06e728bb4a4ef7e188174da21390e9f65238938
Ruchit Darji <darjiruchit02@gmail.com>
2021-09-19 21:31:21 +0530

    Initial launch

--- src/main/java/com/topnotch/demo/controllers/SampleController.java
+++ src/main/java/com/topnotch/demo/controllers/SampleController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.topnotch.demo.dtos.DocUploadResponse;
 import com.topnotch.demo.models.EmployeeDetails;
@@ -39,6 +40,13 @@ public class SampleController {
 	
 	@Value("${com.topnotch.properties.gatewayservice.transferprotocol}")
 	private String TRANSFER_PROTOCOL;
+	
+	@GetMapping("/healthCheck")
+	@ResponseBody
+	public String checkLiveness(){
+		
+		return "App is doing fine!";
+	}
 	
 	@GetMapping("/homePage")
 	public String displayHomepage(ServerHttpRequest request, Model model) {
